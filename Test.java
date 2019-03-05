95df17671c1f4d65daf9cabd40f7c594453a5297
wzy <zyu_work@163.com>
2019-03-05 15:18:38 +0800

    vscode test

--- Test.java
+++ Test.java
@@ -2,6 +2,9 @@ class Test {
 	public static void main() {
 		System.out.println("hello world!!!");
 		// edited by wzy from vim
+
+		System.out.println("hello git");
+		//edited by wzy from vscode
 	}
 }
 
